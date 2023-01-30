9c9f583f790255e46bb89fc36700b9ccd6838c33
Five Pandas <amandapaul1223@gmail.com>
2023-01-29 17:22:15 -0700

    Starter Code

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+// Import our modular routers for /tips and /feedback
+const notesRouter = require('./notes');
+const apiNotesRouter = require('./api/notes') ;
+const indexHtmlRouter = require('.*');
+const app = express();
+
+app.use('/notes', notesRouter);
+app.use('/api/notes', apiNotesRouter);
+app.use('*', indexHtmlRouter);
+module.exports = app;
